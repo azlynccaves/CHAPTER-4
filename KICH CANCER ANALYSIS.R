0d8e511584ad7d35aa36ec6e3efcf0b56f4f0b5a
Azlyn Caves <azlyn.caves@okstate.edu>
2022-05-06 01:53:26 +0000

    KICH commint, all code is there including genes, no ggplot is appearing

--- KICH CANCER ANALYSIS.R
+++ KICH CANCER ANALYSIS.R
@@ -9,36 +9,34 @@ force=TRUE
 
 library(FirebrowseR)
 library(ggplot2)
-
 ##loads the FirebrowseR library and the ggplot2 library
 ##For line of code below-API responded with code 404. Your query might be to big
-##DO NOT USE LINE 15 Needed spaces line 17 worked line 15 did not
+
 cohorts=FirebrowseR::Metadata.Cohort(format = "csv")
 ##Needed spaces line 17 worked line 15 did not
-
 cohorts = FirebrowseR::Metadata.Cohorts(format = "csv")
-cancer.Type=cohorts[grep ("breast", cohorts$description, ignore.case = T), 1]
+cancer.Type=cohorts[grep ("kidney", cohorts$description, ignore.case = T), 1]
 print(cancer.Type)
-##[1] "BRCA"
-brca.Pats=FirebrowseR::Samples.Clinical(cohort=cancer.Type, format = "tsv")
-dim(brca.Pats)
+
+KICH.Pats=FirebrowseR::Samples.Clinical(cohort=cancer.Type, format = "tsv")
+dim(KICH.Pats)
 ##[1] 150. 111
 all.Recieved=FALSE
 page.Counter=1
 page.size=150
-brca.Pats=list()
-while(all.Recieved==FALSE){brca.Pats[[page.Counter]]=Samples.Clinical(format = "csv", cohort = cancer.Type, page_size = page.size, page=page.Counter)}
-if(page.counter>1) colnames(brca.Pats[[page.Counter]])=colnames(brac.Pats[[page.Counter-1]])
-if(nrow(brca.Pats[[page.Counter]])<page.sizes) {all.Recieved=TRUE} else{page.Counter=page.Counter-1}
-brca.Pats=do.call(rbind,brca.Pats)
-dim(brca.Pats)
-brca.Pats=brca.Pats[ which(brca.Pats$vital_status=="dead"),]
-diff.Exp.Genes=c("ESR1","GATA3","XBP1","FOXA1","ERBB2","GRB7","EGFR","FOXC1","MYC")
+KICH.Pats=list()
+while(all.Recieved==FALSE){KICH.Pats[[page.Counter]]=Samples.Clinical(format = "csv", cohort = cancer.Type, page_size = page.size, page=page.Counter)}
+if(page.counter>1) colnames(KICH.Pats[[page.Counter]])=colnames(KICH.Pats[[page.Counter-1]])
+if(nrow(KICH.Pats[[page.Counter]])<page.sizes) {all.Recieved=TRUE} else{page.Counter=page.Counter-1}
+KICH.Pats=do.call(rbind,brca.Pats)
+dim(KICH.Pats)
+KICH.Pats=KICH.Pats[ which(KICH.Pats$vital_status=="dead"),]
+diff.Exp.Genes=c("TP53","CDC27","PABPC1","PTEN","AMAC1L3","ZNF814","MTMR9","RB1")
 all.Found=FALSE
 page.Counter=1
 mRNA.Exp=list()
 page.size=2000
-while(all.Found==FALSE){mRNA.Exp[[page.Counter]]=FirebrowseR::Samples.mRNASeq(format="csv",gene=diff.Exp.Genes,cohort="BRCA", tcga_participant_barcode = brca.Pats$tcga_participant_barcode,page_size = page.Size, page=page.Counter)}
+while(all.Found==FALSE){mRNA.Exp[[page.Counter]]=FirebrowseR::Samples.mRNASeq(format="csv",gene=diff.Exp.Genes,cohort="KICH", tcga_participant_barcode = brca.Pats$tcga_participant_barcode,page_size = page.Size, page=page.Counter)}
 if(nrow(mRNA.Exp[[page.Counter]]) < page.size) all.Found = TRUE else page.Counter = page.Counter + 1
 
 
@@ -58,4 +56,9 @@ p=ggplot(mRNA.Exp, aes(factor(gene), z.score))
 p+ geom_boxplot(aes(fill=factor(sample_type))) + scale_y_continuous(limits=c(-1,5)) + scale_fill_discrete(name="Tissue")
 
 
-#all code ran w/o errors but never showed a ggplot and unsure of the cause due to no error
+
+#creating ggplot data frame 
+
+df("TP53","CDC27","PABPC1","PTEN","AMAC1L3","ZNF814","MTMR9","RB1")
+
+##data frame not showing in environment but having no error
